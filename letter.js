af76a8a10055257d26a4488714709e152c7b3f69
Christi Savino <c.savino@ymail.com>
2016-09-28 14:23:04 -0400

    making game.js and letter.js constructor only files

--- letter.js
+++ letter.js
@@ -1,36 +1,20 @@
-// the random word constructor is in game.js
-
-// Invokes the randomWord generator in game.js to create the random word
 // Creates a string of all underscores to display to the user to begin the game
-// currentWord is the array that the correct letters are pushed into
-// displayWord is the string version that is displayed
-var randomWord = require('./game.js');
-function HangmanWord(){
+// currentWord is the array that the correct letters are pushed into, displayWord is the string version that is displayed
+
+function HangmanWord(randomWord){
 	this.currentWord = [];
 	this.displayWord = '';
 	this.letterStr = '';
 	this.lettersLeft = 0;
-	this.nodeRandomWord = '';
-	this.makeRandomWord = function(){
-		this.gameRandomWord = new randomWord();
-		this.gameRandomWord.chooseWord();
-		this.nodeRandomWord = this.gameRandomWord.word;
-		console.log('the random word is',this.nodeRandomWord);
-	},
+	this.nodeRandomWord = randomWord;
+	//Make an array with all Underscores the length of the random word
 	this.makeHangmanWord = function(){
 		this.lettersLeft = this.nodeRandomWord.length;
-		for (var i = 0; i < this.nodeRandomWord.length; i++){
-			//Make an array with all Underscores the length of the random word
+		for (var i = 0; i < this.nodeRandomWord.length; i++) {
 			this.currentWord[i] = '_';
 		}
 		this.displayWord = this.currentWord.join(' ');
-		//console.log('displayWord in letterConst.js ',this.displayWord);
 	}
 } //end of hangmanWord constructor
-var gameHangmanWord = new HangmanWord();
 
-gameHangmanWord.makeRandomWord();
-gameHangmanWord.makeHangmanWord();
-
-//module.exports = gameRandomWord;
-module.exports = gameHangmanWord;
+module.exports = HangmanWord;
--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 //var word = require('./word.js');
 var inquirer = require('inquirer');
-var gameHangmanWord = require('./letter.js');
+var makeRandomWord = require('./game.js');
+var HangmanWord = require('./letter.js');
 var userGuess;
 //log it to be sure the scope is correct and the module.exports and the require is correct
-
-console.log("\nLet's play Node Package Hangman. \nGuess a letter in one of the most popular node packages ");
-console.log('Here is your word\n\n',gameHangmanWord.displayWord);
+var makeRandomWord = new makeRandomWord();
+makeRandomWord.chooseWord();
+var HangmanWord = new HangmanWord(makeRandomWord.RandomWord );
+HangmanWord.makeHangmanWord();
+console.log('\nin main, makeRandWord.RandomWord', makeRandomWord.RandomWord );
+console.log('HangmanWord ',HangmanWord.displayWord); 
+console.log("\nLet's play Node Package Hangman. \nGuess a letter in one of the most popular Node packages ");
+console.log('Here is your word\n\n', HangmanWord.displayWord + '\n');
 
 function prompt() {
 	inquirer.prompt([
