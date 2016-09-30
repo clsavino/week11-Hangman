03feafc5cf16f60155a97c956672874ee6708ff7
Christi Savino <c.savino@ymail.com>
2016-09-30 18:27:45 -0400

    added checking the validity of the input and recursive call to promt user for a letter

--- game.js
+++ game.js
@@ -1,7 +1,3 @@
-var inquirer = require('inquirer');
-
-var HangmanWord = require('./letter.js');
-
 var Setup = {
 	nodePackages: [
 		'twitter',
@@ -21,41 +17,17 @@ var Setup = {
 		],
 	RandomWord: '',
 	gameDisplayWord: '',
-	userGuess: '',
-	greeting: "\nLet's play Node Package Hangman. \nGuess a letter in one of the most popular Node packages ",
+	greeting: "\n  Let's play Node Package Hangman. \nGuess a letter in one of the most popular Node packages ",
 	
 	generateRandomWord : function() {
 		var index = (Math.floor(Math.random()*14) + 1);
 		this.RandomWord = this.nodePackages[index];
 	},
+
 	printGreetings : function() {
 		console.log(this.greeting);
-	},
+	}
 
-	generateDisplayWord : function() {
-		this.HangmanWord = new HangmanWord(this.RandomWord)
-		this.HangmanWord.makeHangmanWord();
-		this.gameDisplayWord = this.HangmanWord.displayWord;
-		console.log('Here is your word\n\n' + this.HangmanWord.displayWord + '\n');
-	},
-
-	promptUser : function() {
-		inquirer.prompt([
-		{
-			type: "input",
-			message: "Enter a letter",
-			name: "letter"
-		}
- 
-		]).then(function (user) {
-			this.userGuess = user.letter;
-			console.log("\nYou entered " + this.userGuess);
-		})
-			.catch(function(e){
-			console.log(e);
-			console.log(e.stack);
-		})
-	}	
 }; //end of Setup
 
 module.exports = Setup; 
--- main.js
+++ main.js
@@ -1,21 +1,62 @@
+var inquirer = require('inquirer');
 var Setup = require('./game.js');
+var HangmanWord = require('./letter.js');
+var CheckGuesses = require('./word.js');
 var GameSetup = Setup;
+var	userGuess = '';
+var displayWord = '';
+var letterInWord = false;
+var gameOver = false;
 
-// var Setup = require('./game.js');
-// var GameSetup = new Setup();
+var promptUser = function() {
+	inquirer.prompt([
+	{
+		type: "input",
+		message: "Enter a letter",
+		name: "letter"
+	}
+
+	]).then(function (user) {
+		userGuess = user.letter;
+
+		letterInWord = GameCheckGuesses.checkTheLetter(userGuess);
+
+		if (letterInWord) {
+			displayWord = GameHangmanWord.updateHangmanWord(userGuess);
+			console.log('\n  Good guess!');
+		} else {
+			gameOver = GameCheckGuesses.updateGuesses(userGuess);
+			console.log('\n  Oops, Sorry not a good choice!');
+		}
+		if (GameHangmanWord.lettersLeft <=0) {
+			gameOver = true;
+		}
+		if (!gameOver) {
+			console.log('\n  ' + displayWord + '\n');
+			promptUser();
+		} else {
+			console.log('  Game Over!');
+		}
+
+	}) //end of .then
+		.catch(function(e){
+		console.log(e);
+		console.log(e.stack);
+	}) // end of inquirer.prompt
+
+} //end of loop
 
 
 GameSetup.generateRandomWord();
-//console.log('GameSetup',GameSetup);
-//console.log('GameSetup.RandomWord', GameSetup.RandomWord);
+console.log('GameSetup.RandomWord', GameSetup.RandomWord);
 
 GameSetup.printGreetings();
 
-GameSetup.generateDisplayWord();
-//console.log('GameSetup',GameSetup);
-//console.log('GameSetup.gameDisplayWord',GameSetup.gameDisplayWord);
+var GameHangmanWord =  new HangmanWord(GameSetup.RandomWord);
 
-var GameCheckGuesses = require('./word.js');
-var GameCheckGuesses = new GameCheckGuesses(GameSetup.RandomWord);
+displayWord = GameHangmanWord.makeHangmanWord();
+console.log('GameHangmanWord',GameHangmanWord);
+var GameCheckGuesses = new CheckGuesses(GameSetup.RandomWord);
+console.log('  Here is your word\n\n  ' + displayWord + '\n');
 
-GameSetup.promptUser();
+promptUser();
